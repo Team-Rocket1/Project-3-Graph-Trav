f72716439731040fa770374923da196aff7e49fb
jessedtran <jessedanieltran@gmail.com>
2025-11-28 22:33:37 -0800

    Established Graph Class using adjacency matrix.
    Established Graph Class using adjacency matrix. Added in the constructor to initialize the graph, using "n" to represent the vertices.

--- Breadth_First_Traversal.java
+++ Breadth_First_Traversal.java
@@ -1,4 +1,16 @@
 import java.util.*;
 public class Breadth_First_Traversal {
-    
+
+    /*Establish Graph Class: Adjacency Matrix Representation*/
+    public static class Graph<E> {
+        private boolean[][] edges;           /*2D array to represent edges. E.g., edges[i][j] is true if there is an edge from vertex i to vertex j*/
+        private E[] labels;                 //Use E to represent labels of vertices.
+
+        @SuppressWarnings("unchecked")  
+        public Graph(int n) {               //Constructor to initialize graph with n vertices. n is the number of vertices.
+            edges = new boolean[n][n];      //Creates a 2D boolean array to represent edges.
+            labels = (E[]) new Object[n];   //Creates an array, (generic type "E") to hold labels of vertices.
+
+        }
+    }    
 }

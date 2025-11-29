61ca9aedd62576dd0815e0aed2a22feb2bf51bf8
jessedtran <jessedanieltran@gmail.com>
2025-11-29 02:13:36 -0800

    Implement neighbors method in Graph class
    Added a method to retrieve neighboring vertices.

--- Breadth_First_Traversal.java
+++ Breadth_First_Traversal.java
@@ -34,5 +34,31 @@ public class Breadth_First_Traversal {
     public boolean isEdge(int source, int target) {     //Returns true if there is an edge from source vertex to target vertex; otherwise, returns false.
         return edges[source][target];
         }
+    
+    /*Returns an array of integers representing the neighboring vertices of the given vertex. 
+    * A neighbor is defined as a vertex that is directly connected to the given vertex by an edge.
+    * For vertex v, we look at row v of the adjacency matrix.
+    * If edges[v][i] is true, then vertex i is a neighbor of vertex v.
+    */
+    public int[] neighbors(int vertex) {
+        int count = 0;
+
+        for (int i = 0; i < size(); i++) {      //First pass: count how many neighbors.
+            if (edges[vertex][i]) {
+                    count++;
+                }
+            }
+
+        int[] result = new int[count];   //Second pass: collect the neighbors.
+        int idx = 0;
+            for (int i = 0; i < size(); i++) {
+                if (edges[vertex][i]) {
+                    result[idx++] = i;
+                }
+            }
+
+            return result;
+        }
 }
+
 

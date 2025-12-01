e2aa77fb22c34bcf2d9664de2cba328f5c2b8d5c
alex <Alex.Oliver.Franklin@gmail.com>
2025-11-30 19:35:56 -0800

    Change Depth_First_Traversal into adjacency list
    Change Depth_First_Traversal class to use adjacency list and simplify methods.

--- Depth_First_Traversal.java
+++ Depth_First_Traversal.java
@@ -1,11 +1,11 @@
 
-public class Depth_First_Traversal<E> implements GraphInterface<E> {
-    private boolean[][] edges; // edges[i][j] = true if there is a vertex i to j
-    private E[] labels;     // labels[i] = label for vertex i
+public class Depth_First_Traversal {
+    private List<list<Integer>> edges;
+    private E[] labels;
 
     public Depth_First_Traversal(int n) {
-        edges = new boolean[n][n];  // all values false by default
-        labels = (E[]) new Object[n]; // all values null by default
+        edges = new ArrayList<>(n);
+        labels = (E[]) new Object[n];
     }
 
     // set the label of a vertex
@@ -18,19 +18,9 @@ public class Depth_First_Traversal<E> implements GraphInterface<E> {
         return labels[vertex];
     }
 
-    // tests if an edge exists
-    public boolean isEdge(int source, int target){
-        return edges[source][target];
-    }
-
     // adds an edge
     public void addEdge(int source, int target){
         edges[source][target] = true;
-    }
-
-    // remove an edge
-    public void removeEdge(int source, int target) {
-        edges[source][target] = false;
     }
 
     // returns the number of vertices in the graph
@@ -39,35 +29,12 @@ public class Depth_First_Traversal<E> implements GraphInterface<E> {
     }
     
     // get a list of neighbors of a given vertex
-    public int[] getNeighbors(int vertex){
-        int count = 0;
-        
-        for (int i = 0; i < labels.length; i++) {
-            if (edges[vertex][i]){
-                count++;
-            }
-        }
-        int[] answer = new int[count];
-        count = 0;
-        for (int i = 0; i < labels.length; i++) {
-            if (edges[vertex][i]) {
-                answer[count++] = i;
-            }
-        }
-        return answer;
+    public List<Integer> getNeighbors(int vertex){
+        return edges.get(vertex);
     }
 
     public static void main(String[] args){
-            Depth_First_Traversal<String> depthGraph = new Depth_First_Traversal<>(5);
-            depthGraph.setLabel(0, "A");
-            depthGraph.setLabel(1, "B");
-            depthGraph.setLabel(2, "C");
-
-            System.out.println(depthGraph.getLabel(0)); // prints label at vertex 0: A
-            System.out.println(depthGraph.getSize()); // prints size of graph: 5
-            depthGraph.addEdge(0,1);
-            System.out.println(depthGraph.getLabel(2));
-            System.out.println(depthGraph.getLabel(3));
+            
         }
     
 }

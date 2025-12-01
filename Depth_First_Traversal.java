0100aeb79bfa312c3784d16f8e7e8257a82bd179
alex <Alex.Oliver.Franklin@gmail.com>
2025-11-30 22:33:43 -0800

    update Depth_First_Traversal

--- Depth_First_Traversal.java
+++ Depth_First_Traversal.java
@@ -1,63 +1,67 @@
+import java.util.*;
 
 public class Depth_First_Traversal {
-    private List<list<Integer>> edges;
-    private E[] labels;
 
-    @SuppressWarnings("unchecked")
-    public Graph(int n) {
-        edges = new ArrayList<>(n);
-        labels = (E[]) new Object[n];
-
-        for (int i = 0; i < n; i++) {
-            edges.add(new ArrayList<>());
+    public static class Graph<E> {
+        private List<List<Integer>> edges;
+        private E[] labels;
+    
+        @SuppressWarnings("unchecked")
+        public Graph(int n) {
+            edges = new ArrayList<>(n);
+            labels = (E[]) new Object[n];
+    
+            for (int i = 0; i < n; i++) {
+                edges.add(new ArrayList<>());
+            }
         }
-    }
-
-    // set the label of a vertex
-    public void setLabel(int vertex, E newLabel) {
-        labels[vertex] = newLabel;
-    }
     
-    // gets the label of a vertex
-    public E getLabel(int vertex){
-        return labels[vertex];
-    }
-
-    // adds an edge
-    public void addEdge(int source, int target){
-        edges[source][target] = true;
-    }
-
-    // returns the number of vertices in the graph
-    public int getSize() {
-        return labels.length;
-    }
+        // set the label of a vertex
+        public void setLabel(int vertex, E newLabel) {
+            labels[vertex] = newLabel;
+        }
     
-    // get a list of neighbors of a given vertex
-    public List<Integer> getNeighbors(int vertex){
-        return edges.get(vertex);
-    }
+        // gets the label of a vertex
+        public E getLabel(int vertex){
+            return labels[vertex];
+        }
+    
+        // adds an edge
+        public void addEdge(int source, int target){
+            edges.get(source).add(target);
+        }
+    
+        // returns the number of vertices in the graph
+        public int getSize() {
+            return labels.length;
+        }
+        
+        // get a list of neighbors of a given vertex
+        public List<Integer> getNeighbors(int vertex){
+            return edges.get(vertex);
+        }
 
-    public List<E> depthFirstTraversal(int origin) {
-        boolean[] visited = new boolean[getSize()];
-        Stack<Integer> stack = new Stack<>();
-        List<E> order = new ArrayList,>();
-
-        stack.push(origin);
-
-        while (!stack.isEmpty()) {
-            int v = stack.pop();
-
-            if (!visited[v]) {
-                visited[v] = true;
-                order.add(getLabel(v));
-
-                // push neighbors in reverse order for natural traversal
-                List<Integer> nbrs = getNeighbors(v);
-                for (int i = nbrs.size() - 1; i >= 0; i--) {
-                    int n = nbrs.get(i);
-                    if (!visited[n]) {
-                        stack.push(n);
+        public List<E> depthFirstTraversal(int origin) {
+            boolean[] visited = new boolean[getSize()];
+            Stack<Integer> stack = new Stack<>();
+            List<E> order = new ArrayList,>();
+    
+            stack.push(origin);
+    
+            while (!stack.isEmpty()) {
+                int v = stack.pop();
+    
+                if (!visited[v]) {
+                    visited[v] = true;
+                    order.add(getLabel(v));
+    
+                    // push neighbors in reverse order for natural traversal
+                    List<Integer> nbrs = getNeighbors(v);
+                    for (int i = nbrs.size() - 1; i >= 0; i--) {
+                        int n = nbrs.get(i);
+                        if (!visited[n]) {
+                            stack.push(n);
+                        }
                     }
                 }
             }
@@ -101,7 +105,7 @@ public class Depth_First_Traversal {
         addEdge(depthGraph, indexOf, 'I', 'F');
 
         int origin = indexOf.get('A');
-        List<Character> order = depthgraph.depthFirstTraversal(origin);
+        List<Character> order = depthGraph.depthFirstTraversal(origin);
 
         System.out.print("Depth-first traversal starting at A: ");
         for (char c : order) {
